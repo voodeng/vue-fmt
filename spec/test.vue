0c2967185e99157cbac0eb8f78eb46733290afae
Voodeng <voodeng@gmail.com>
2018-02-03 06:34:54 +0800

    feat: use JSbeautify and Prettier

---
+++ spec/test.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app">
+		<transition name="fade" modeSlit="out-in">
+			<router-view class="view" />
+		</transition>
+	</div>
+</template>
+
+<style lang='scss'>
+	@import './style/App.scss';
+	#app {
+		width: 100%; // width: auto;
+		padding: 1px;
+	}
+</style>
+
+<script lang="ts">
+	export default {
+		name: 'App',
+		created() {
+			// do something after creating vue instance
+		}
+	}
+</script>
